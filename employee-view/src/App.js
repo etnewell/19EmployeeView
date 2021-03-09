4e3e05f6d990bb013562890f76b0a9f4c4c9e640
etnewell <ethannewell@me.com>
2021-03-08 18:39:44 -0800

    I got everything to display properly, the filtering function now works

--- employee-view/src/App.js
+++ employee-view/src/App.js
@@ -1,32 +1,76 @@
 import React, { Component } from 'react';
 import EmployeeCard from './components/EmployeeCard';
 import Wrapper from './components/Wrapper';
-import employees from './employees.json';
-import Title from './components/title';
+import employeeList from './employees.json';
+import Title from './components/Title';
 import './index.css';
 
 class App extends Component {
 
   state = {
-    employees
+    employees: employeeList,
+    search: ""
   };
 
-  filterEmployees = () => {
+  filterEmployees = name => {
 
+    var empAry = [...this.state.employees];
+    var searchResults = [];
+    let i;
+    for (i=0; i < empAry.length; i++) {
+      if (empAry[i].firstname === this.state.search){
+        searchResults.push(empAry[i])
+      } else if (empAry[i].lastname === this.state.search){
+        searchResults.push(empAry[i])
+      } else if (empAry[i].firstname + " " + empAry[i].lastname === this.state.search){
+        searchResults.push(empAry[i])
+      } else if (empAry[i].employeeId === parseInt(this.state.search)){
+        searchResults.push(empAry[i])
+      }
+    }
+
+    this.setState({employees: searchResults})
+
+    if (searchResults.length === 0){
+      alert("Make sure you have the correct spelling of the name!")
+      this.setState({employees: employeeList})
+    }
   };
+
+  handleInput = event => {
+    this.setState({search: event.target.value})
+  };
+
+  reverseSearch = () => {
+    this.setState({employees: employeeList})
+  }
+
+  // enterSearch = () => {
+  //   // console.log(this.state.search)
+  //   filterEmployees(this.state.search)
+  // }
 
   render() {
     return (
       <Wrapper>
-        <Title>Employee Public View</Title>
+        <Title>Employee Public Information View</Title>
+        <div className="search">
+        <input type="text" placeholder="Search Employees" className="searchInput" onChange={this.handleInput}></input>
+        <button onClick={this.filterEmployees} className="btn-search">Go!</button>
+        <button onClick={this.reverseSearch} className="btn-search">ShowAll</button>
+        </div>
+        <div className="employeeCards">
         {this.state.employees.map(employee =>(
           <EmployeeCard
           employeeId={employee.employeeId}
-          name={employee.name}
+          key={employee.employeeId}
+          firstname={employee.firstname}
+          lastname={employee.lastname}
           title={employee.title}
           department={employee.department}
           />
           ))}
+          </div>
       </Wrapper>
    );
   }
--- employee-view/src/components/EmployeeCard/index.js
+++ employee-view/src/components/EmployeeCard/index.js
@@ -4,10 +4,13 @@ import "./style.css";
 const EmployeeCard = props => {
     return (
         <div className="card">
-            <h1 class ="employeeName">{props.name}</h1>
+            <h1 class ="employeeName">{props.firstname} {props.lastname}</h1>
             <ul className="employeeInfo">
-                <li className="empName">{props.empName}</li>
+                <li className="empTitle">Employee Title: {props.title}</li>
+                <li className="empId">Employee ID: {props.employeeId}</li>
+                <li className="empDept">Employee Department: {props.department}</li>
             </ul>
         </div>
     )
 }
+export default EmployeeCard;
